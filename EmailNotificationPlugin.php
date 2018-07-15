cdd49d7e38910add4d34a47372f183c8d1e9d168
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2018-07-15 13:12:07 +0200

    Add files via upload

---
+++ EmailNotificationPlugin.php
@@ -0,0 +1,185 @@
+<?php
+
+/**
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright Daniele Binaghi, 2018
+ * @package EmailNotification
+ */
+
+/**
+ * SimpleContactForm plugin class
+ *
+ * @copyright Daniele Binaghi, 2018
+ * @package EmailNotification
+ */
+ 
+// Define Constants
+define('EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_SUBJECT', __('New Item added'));
+define('EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_MESSAGE', __('A new Item has been added to the Omeka repository.'));
+define('EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_SUBJECT', __('New Collection added'));
+define('EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_MESSAGE', __('A new Collection has been added to the Omeka repository.'));
+define('EMAIL_NOTIFICATION_MESSAGE_SENT', __('Admin/editors have been notified about the new addition.'));
+
+class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
+{
+    protected $_hooks = array(
+        'install',
+        'uninstall',
+        'initialize',
+		'config',
+		'config_form',
+		'after_save_item',
+		'after_save_collection'
+    );
+
+    public function hookInstall()
+    {
+        set_option('email_notification_new_item', '0');    
+        set_option('email_notification_new_item_email_subject', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_SUBJECT);
+        set_option('email_notification_new_item_email_message', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_MESSAGE);
+        set_option('email_notification_new_collection', '0');    
+        set_option('email_notification_new_collection_email_subject', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_SUBJECT);
+        set_option('email_notification_new_collection_email_message', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_MESSAGE);
+        set_option('email_notification_recipient_address', get_option('administrator_email'));    
+        set_option('email_notification_notify_editors', '0');
+		set_option('email_notification_message_sent', '0');
+ 	}
+
+    public function hookUninstall()
+    {
+        delete_option('email_notification_new_item');
+        delete_option('email_notification_new_item_email_subject');
+        delete_option('email_notification_new_item_email_message');
+        delete_option('email_notification_new_collection');	
+        delete_option('email_notification_new_collection_email_subject');
+        delete_option('email_notification_new_collection_email_message');
+        delete_option('email_notification_recipient_address');
+        delete_option('email_notification_notify_editors');    
+		delete_option('email_notification_message_sent');
+     }
+
+    public function hookInitialize()
+    {
+        add_translation_source(dirname(__FILE__) . '/languages');
+    }
+	
+	public function hookConfig($args)
+    {
+        $post = $args['post'];
+        set_option('email_notification_new_item', $post['email_notification_new_item']);
+        set_option('email_notification_new_item_email_subject',  $post['email_notification_new_item_email_subject']);
+        set_option('email_notification_new_item_email_message',  $post['email_notification_new_item_email_message']);
+        set_option('email_notification_new_collection',  $post['email_notification_new_collection']);
+        set_option('email_notification_new_collection_email_subject',  $post['email_notification_new_collection_email_subject']);
+        set_option('email_notification_new_collection_email_message',  $post['email_notification_new_collection_email_message']);
+        set_option('email_notification_recipient_address',  $post['email_notification_recipient_address']);
+        set_option('email_notification_notify_editors', $post['email_notification_notify_editors']);
+		set_option('email_notification_message_sent', $post['email_notification_message_sent']);
+    }
+	
+	public function hookConfigForm()
+    {
+        include 'config_form.php';
+    }
+	
+	public function hookAfterSaveItem($args)
+    {
+		if (!$args['post'] || $args['insert'] != 1) {
+			return;
+		}
+        $recordId = $args['record']['id'];
+		
+		$this->sendNotification('item', $recordId);
+    }
+
+	public function hookAfterSaveCollection($args)
+    {
+		if (!$args['post'] || $args['insert'] != 1) {
+			return;
+		}
+        $recordId = $args['record']['id'];
+
+		$this->sendNotification('collection', $recordId);
+    }
+
+	public function sendNotification($type, $recordId) {
+		$recipientAddress = get_option('email_notification_recipient_address');
+		$notifyEditors = get_option('email_notification_notify_editors');
+		$bodyHtml = '';
+		$subject = '';
+		$bMessageSent = false;
+
+		
+		if ($recipientAddress != '' || $notifyEditors) {
+			// creates e-mail elements
+			if ($type == 'item' && get_option('email_notification_new_item')) {
+				$O_R_AR = get_record_by_id('item', $recordId);
+				$bodyHtml = get_option('email_notification_new_item_email_message');
+				$title = metadata($O_R_AR, array('Dublin Core', 'Title'));
+				$adder = current_user()->name;
+				$status = ($O_R_AR->public == 1 ? __('public') : __('private'));
+				if ($title != '') {
+					$bodyHtml .= '<p>' . __('The title of the new Item, added by <b>%s</b>,' , $adder) . ' ' . __('is <b>%s</b>.', $title) . '</p><p>' .  __('The status of the new Item is <b>%s</b>', $status) . '.</p><p><a href="' . $_SERVER['HTTP_HOST'] . '/admin/items/show/id/' . $recordId . '">' . __('Please check that all data are correct!') . '</a></p>';
+				} else {
+					$bodyHtml .= '<p>' . __('The new Item, added by <b>%s</b>, does not have a title yet.', $adder) . '</p><p>' .  __('The status of the new Item is <b>%s</b>', $status) . '.</p><p><a href="' . $_SERVER['HTTP_HOST'] . '/admin/items/show/id/' . $recordId . '">' . __('Please check that all data are correct!') . '</a></p>';
+					$message = __('No title has been provided for the new Item.');
+					$flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+					$flash->addMessage($message, 'error');
+				}
+				$subject =  get_option('email_notification_new_item_email_subject');
+			} elseif ($type == 'collection' && get_option('email_notification_new_collection')) {
+				$O_R_AR = get_record_by_id('collection', $recordId);
+				$bodyHtml = get_option('email_notification_new_collection_email_message');
+				$title = metadata($O_R_AR, array('Dublin Core', 'Title'));
+				$adder = current_user()->name;
+				$status = ($O_R_AR->public == 1 ? __('public') : __('private'));
+				if ($title != '') {
+					$bodyHtml .= '<p>' . __('The title of the new Collection, added by <b>%s</b>,', $adder) . ' ' . __('is <b>%s</b>.', $title) . '</p><p>' . __('The status of the new Collection is <b>%s</b>', $status) . '.</p><p><a href="' . $_SERVER['HTTP_HOST'] . '/admin/collections/show/id/' . $recordId . '">' . __('Please check that all data are correct!') . '</a></p>';
+				} else {
+					$bodyHtml .= '<p>' . __('The new Collection, added by <b>%s</b>, does not have a title yet.', $adder) . '</p><p>' .  __('The status of the new Collection is <b>%s</b>', $status) . '.</p><p><a href="' . $_SERVER['HTTP_HOST'] . '/admin/items/show/id/' . $recordId . '">' . __('Please check that all data are correct!') . '</a></p>';
+					$message = __('No title has been provided for the new Collection.');
+					$flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+					$flash->addMessage($message, 'error');
+				}
+				$subject =  get_option('email_notification_new_collection_email_subject');
+			}
+			
+			if ($bodyHtml != '' && $subject != '') {
+				if ($recipientAddress != '') {
+					// sends e-mail to recipient address
+					$email = new Zend_Mail('utf-8');
+					$email->setBodyHtml($bodyHtml);
+					$email->setFrom(get_option('administrator_email'));
+					$email->addTo($recipientAddress);
+					$email->setSubject($subject);
+					$email->send();
+					$bMessageSent = true;
+				}
+				
+				if ($notifyEditors) {
+					// sends e-mail to all users with general 'edit' privilege
+					$acl = get_acl();
+					$users = get_db()->getTable('User')->findAll();
+					foreach ($users as $key => $user):
+						if ($acl->isAllowed($user, new Item, 'edit')):
+							$email = new Zend_Mail('utf-8');
+							$email->setBodyHtml($bodyHtml);
+							$email->setFrom(get_option('administrator_email'));
+							$email->addTo($user->email);
+							$email->setSubject($subject);
+							$email->send();
+							$bMessageSent = true;
+						endif;
+					endforeach;
+				}
+				
+				if (get_option('email_notification_message_sent') && $bMessageSent) {
+					$message = __(EMAIL_NOTIFICATION_MESSAGE_SENT);
+					$flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+					$flash->addMessage($message, 'success');
+				}
+			}
+		}
+	}
+}
---
+++ config_form.php
@@ -0,0 +1,137 @@
+<?php echo js_tag('vendor/tinymce/tinymce.min'); ?>
+<script type="text/javascript">
+jQuery(document).ready(function () {
+	Omeka.wysiwyg({
+		selector: '.html-editor'
+	});
+});
+</script>
+
+<?php
+$new_item						= get_option('email_notification_new_item');
+$new_item_email_subject			= get_option('email_notification_new_item_email_subject');
+$new_item_email_message 		= get_option('email_notification_new_item_email_message');
+$new_collection					= get_option('email_notification_new_collection');	
+$new_collection_email_subject	= get_option('email_notification_new_collection_email_subject');
+$new_collection_email_message 	= get_option('email_notification_new_collection_email_message');
+$recipient_address				= get_option('email_notification_recipient_address');
+$notify_editors					= get_option('email_notification_notify_editors');
+$message_sent					= get_option('email_notification_message_sent');
+$view = get_view();
+?>
+
+<h2><?php echo __('New Item addition'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_new_item', __('Enable notification')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, sends a notification every time a new Item is added.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('email_notification_new_item', $new_item, null, array('1', '0')); ?>
+	</div>
+</div>
+
+ <div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_new_item_email_subject', __('E-mail subject')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The subject line for the notification e-mail sent to the recipients.'); ?>
+		</p>
+		<?php echo $view->formText('email_notification_new_item_email_subject', $new_item_email_subject); ?>
+	</div>
+</div>
+
+ <div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_new_item_email_message', __('E-mail message')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The beginning of the notification message sent to the recipients.') ?>
+		</p>
+		<?php echo $view->formTextarea('email_notification_new_item_email_message', $new_item_email_message, array('rows' => '10', 'cols' => '60', 'class' => array('html-editor'))); ?>
+	</div>
+</div>
+
+<h2><?php echo __('New Collection addition'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_new_collection', __('Enable notification')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, sends a notification every time a new Collection is added.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('email_notification_new_collection', $new_collection, null, array('1', '0')); ?>
+	</div>
+</div>
+
+ <div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_new_collection_email_subject', __('E-mail subject')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The subject line for the notification e-mail sent to the recipients.'); ?>
+		</p>
+		<?php echo $view->formText('email_notification_new_collection_email_subject', $new_collection_email_subject); ?>
+	</div>
+</div>
+
+ <div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_new_collection_email_message', __('E-mail message')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The beginning of the notification message sent to the recipients.'); ?>
+		</p>
+		<?php echo $view->formTextarea('email_notification_new_collection_email_message', $new_collection_email_message, array('rows' => '10', 'cols' => '60', 'class' => array('html-editor'))); ?>
+	</div>
+</div>
+
+<h2><?php echo __('Recipients'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_recipient_address', __('Recipient e-mail address')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('The address that will receive the notification.'); ?>
+		</p>
+		<?php echo $view->formText('email_notification_recipient_address', $recipient_address); ?>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_notify_editors', __('Notify editors')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, system sends a notification of new submissions also to users with editing role.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('email_notification_notify_editors', $notify_editors, null, array('1', '0')); ?>
+	</div>
+</div>
+
+<h2><?php echo __('Alerts'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_message_sent', __('Alert contributor about notification')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, system shows an alert informing contributor about the notification.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('email_notification_message_sent', $message_sent, null, array('1', '0')); ?>
+	</div>
+</div>
