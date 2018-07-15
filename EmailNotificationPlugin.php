7acbd0c52b19628dad4bc723e9e5d627fc06bd21
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2018-07-15 18:09:47 +0200

    Update EmailNotificationPlugin.php
    Polished code

--- EmailNotificationPlugin.php
+++ EmailNotificationPlugin.php
@@ -7,13 +7,6 @@
  * @package EmailNotification
  */
 
-/**
- * SimpleContactForm plugin class
- *
- * @copyright Daniele Binaghi, 2018
- * @package EmailNotification
- */
- 
 // Define Constants
 define('EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_SUBJECT', __('New Item added'));
 define('EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_MESSAGE', __('A new Item has been added to the Omeka repository.'));
@@ -23,85 +16,83 @@ define('EMAIL_NOTIFICATION_MESSAGE_SENT', __('Admin/editors have been notified a
 
 class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    protected $_hooks = array(
-        'install',
-        'uninstall',
-        'initialize',
+	protected $_hooks = array(
+		'install',
+		'uninstall',
+		'initialize',
 		'config',
 		'config_form',
 		'after_save_item',
 		'after_save_collection'
-    );
+	);
 
-    public function hookInstall()
-    {
-        set_option('email_notification_new_item', '0');    
-        set_option('email_notification_new_item_email_subject', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_SUBJECT);
-        set_option('email_notification_new_item_email_message', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_MESSAGE);
-        set_option('email_notification_new_collection', '0');    
-        set_option('email_notification_new_collection_email_subject', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_SUBJECT);
-        set_option('email_notification_new_collection_email_message', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_MESSAGE);
-        set_option('email_notification_recipient_address', get_option('administrator_email'));    
-        set_option('email_notification_notify_editors', '0');
+	public function hookInstall()
+	{
+		set_option('email_notification_new_item', '0');    
+		set_option('email_notification_new_item_email_subject', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_SUBJECT);
+		set_option('email_notification_new_item_email_message', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_MESSAGE);
+		set_option('email_notification_new_collection', '0');    
+		set_option('email_notification_new_collection_email_subject', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_SUBJECT);
+		set_option('email_notification_new_collection_email_message', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_MESSAGE);
+		set_option('email_notification_recipient_address', get_option('administrator_email'));    
+		set_option('email_notification_notify_editors', '0');
 		set_option('email_notification_message_sent', '0');
- 	}
+	}
 
-    public function hookUninstall()
-    {
-        delete_option('email_notification_new_item');
-        delete_option('email_notification_new_item_email_subject');
-        delete_option('email_notification_new_item_email_message');
-        delete_option('email_notification_new_collection');	
-        delete_option('email_notification_new_collection_email_subject');
-        delete_option('email_notification_new_collection_email_message');
-        delete_option('email_notification_recipient_address');
-        delete_option('email_notification_notify_editors');    
+	public function hookUninstall()
+	{
+		delete_option('email_notification_new_item');
+		delete_option('email_notification_new_item_email_subject');
+		delete_option('email_notification_new_item_email_message');
+		delete_option('email_notification_new_collection');	
+		delete_option('email_notification_new_collection_email_subject');
+		delete_option('email_notification_new_collection_email_message');
+		delete_option('email_notification_recipient_address');
+		delete_option('email_notification_notify_editors');    
 		delete_option('email_notification_message_sent');
-     }
+	}
 
-    public function hookInitialize()
-    {
-        add_translation_source(dirname(__FILE__) . '/languages');
-    }
+    	public function hookInitialize()
+    	{
+        	add_translation_source(dirname(__FILE__) . '/languages');
+    	}
 	
-	public function hookConfig($args)
-    {
-        $post = $args['post'];
-        set_option('email_notification_new_item', $post['email_notification_new_item']);
-        set_option('email_notification_new_item_email_subject',  $post['email_notification_new_item_email_subject']);
-        set_option('email_notification_new_item_email_message',  $post['email_notification_new_item_email_message']);
-        set_option('email_notification_new_collection',  $post['email_notification_new_collection']);
-        set_option('email_notification_new_collection_email_subject',  $post['email_notification_new_collection_email_subject']);
-        set_option('email_notification_new_collection_email_message',  $post['email_notification_new_collection_email_message']);
-        set_option('email_notification_recipient_address',  $post['email_notification_recipient_address']);
-        set_option('email_notification_notify_editors', $post['email_notification_notify_editors']);
+    	public function hookConfig($args)
+    	{
+        	$post = $args['post'];
+		set_option('email_notification_new_item', $post['email_notification_new_item']);
+		set_option('email_notification_new_item_email_subject',  $post['email_notification_new_item_email_subject']);
+		set_option('email_notification_new_item_email_message',  $post['email_notification_new_item_email_message']);
+		set_option('email_notification_new_collection',  $post['email_notification_new_collection']);
+		set_option('email_notification_new_collection_email_subject',  $post['email_notification_new_collection_email_subject']);
+		set_option('email_notification_new_collection_email_message',  $post['email_notification_new_collection_email_message']);
+		set_option('email_notification_recipient_address',  $post['email_notification_recipient_address']);
+        	set_option('email_notification_notify_editors', $post['email_notification_notify_editors']);
 		set_option('email_notification_message_sent', $post['email_notification_message_sent']);
-    }
+    	}
 	
-	public function hookConfigForm()
-    {
-        include 'config_form.php';
-    }
+    	public function hookConfigForm()
+    	{
+        	include 'config_form.php';
+	}
 	
 	public function hookAfterSaveItem($args)
-    {
+    	{
 		if (!$args['post'] || $args['insert'] != 1) {
 			return;
 		}
-        $recordId = $args['record']['id'];
-		
+        	$recordId = $args['record']['id'];
 		$this->sendNotification('item', $recordId);
-    }
+    	}
 
 	public function hookAfterSaveCollection($args)
-    {
+    	{
 		if (!$args['post'] || $args['insert'] != 1) {
 			return;
 		}
-        $recordId = $args['record']['id'];
-
+        	$recordId = $args['record']['id'];
 		$this->sendNotification('collection', $recordId);
-    }
+	}
 
 	public function sendNotification($type, $recordId) {
 		$recipientAddress = get_option('email_notification_recipient_address');
@@ -109,7 +100,6 @@ class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
 		$bodyHtml = '';
 		$subject = '';
 		$bMessageSent = false;
-
 		
 		if ($recipientAddress != '' || $notifyEditors) {
 			// creates e-mail elements
