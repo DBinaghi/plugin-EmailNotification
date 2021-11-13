71f3b9dbdcadb4433f34f016a5a3a168e134b358
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-13 16:58:33 +0100

    Added new option, capitalized strings

--- config_form.php
+++ config_form.php
@@ -1,5 +1,4 @@
 <?php echo js_tag('vendor/tinymce/tinymce.min'); ?>
-
 <script type="text/javascript">
 	jQuery(document).ready(function () {
 		Omeka.wysiwyg({
@@ -12,11 +11,11 @@
 	$view = get_view();
 ?>
 
-<h2><?php echo __('New Item addition'); ?></h2>
+<h2><?php echo __('New Item Addition'); ?></h2>
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_item', __('Enable notification')); ?>
+		<?php echo $view->formLabel('email_notification_new_item', __('Enable Notification')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -28,7 +27,7 @@
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_item_email_subject', __('E-mail subject')); ?>
+		<?php echo $view->formLabel('email_notification_new_item_email_subject', __('E-mail Subject')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -40,7 +39,7 @@
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_item_email_message', __('E-mail message')); ?>
+		<?php echo $view->formLabel('email_notification_new_item_email_message', __('E-mail Message')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -50,13 +49,11 @@
 	</div>
 </div>
 
-<p>&nbsp;</p>
-
-<h2><?php echo __('New Collection addition'); ?></h2>
+<h2><?php echo __('New Collection Addition'); ?></h2>
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_collection', __('Enable notification')); ?>
+		<?php echo $view->formLabel('email_notification_new_collection', __('Enable Notification')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -68,7 +65,7 @@
 
  <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_collection_email_subject', __('E-mail subject')); ?>
+		<?php echo $view->formLabel('email_notification_new_collection_email_subject', __('E-mail Subject')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -80,7 +77,7 @@
 
  <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_collection_email_message', __('E-mail message')); ?>
+		<?php echo $view->formLabel('email_notification_new_collection_email_message', __('E-mail Message')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -90,14 +87,12 @@
 	</div>
 </div>
 
-<p>&nbsp;</p>
-
-<h2><?php echo __('New Exhibit addition'); ?></h2>
+<h2><?php echo __('New Exhibit Addition'); ?></h2>
 
 <?php if (plugin_is_active('ExhibitBuilder')): ?>
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_exhibit', __('Enable notification')); ?>
+		<?php echo $view->formLabel('email_notification_new_exhibit', __('Enable Notification')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -109,7 +104,7 @@
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_exhibit_email_subject', __('E-mail subject')); ?>
+		<?php echo $view->formLabel('email_notification_new_exhibit_email_subject', __('E-mail Subject')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -121,7 +116,7 @@
 
  <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_exhibit_email_message', __('E-mail message')); ?>
+		<?php echo $view->formLabel('email_notification_new_exhibit_email_message', __('E-mail Message')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -135,7 +130,7 @@
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_new_exhibit_email_message', __('Plugin not installed/active')); ?>
+		<?php echo $view->formLabel('email_notification_new_exhibit_email_message', __('Plugin Unavailable')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -149,7 +144,7 @@
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_recipient_address', __('Recipient e-mail address(es)')); ?>
+		<?php echo $view->formLabel('email_notification_recipient_address', __('Recipient Address')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -161,7 +156,7 @@
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_notify_editors', __('Notify editors')); ?>
+		<?php echo $view->formLabel('email_notification_notify_editors', __('Notify Editors')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
@@ -171,16 +166,30 @@
 	</div>
 </div>
 
+<h2><?php echo __('Publishing'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('email_notification_notify_owner', __('Notify Owner')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, system sends a notification to original owner when an Item / Collection / Exhibit they contributed is made public.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('email_notification_notify_owner', get_option('email_notification_notify_owner'), null, array('1', '0')); ?>
+	</div>
+</div>
+
 <h2><?php echo __('Alerts'); ?></h2>
 
 <div class="field">
 	<div class="two columns alpha">
-		<?php echo $view->formLabel('email_notification_message_sent', __('Alert contributor')); ?>
+		<?php echo $view->formLabel('email_notification_notification_alert', __('Notification Alert')); ?>
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
-			<?php echo __('If checked, system shows an alert informing contributor about the notification.'); ?>
+			<?php echo __('If checked, system shows an alert informing contributor that notifications have been sent.'); ?>
 		</p>
-		<?php echo $view->formCheckbox('email_notification_message_sent', get_option('email_notification_message_sent'), null, array('1', '0')); ?>
+		<?php echo $view->formCheckbox('email_notification_notification_alert', get_option('email_notification_notification_alert'), null, array('1', '0')); ?>
 	</div>
 </div>
