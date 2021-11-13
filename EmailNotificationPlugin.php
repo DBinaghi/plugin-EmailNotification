dee359f3299371c17dffe60192bdab5062db7c64
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-11-13 16:59:54 +0100

    Added new option for owner notification, optimized and tidied up code

--- EmailNotificationPlugin.php
+++ EmailNotificationPlugin.php
@@ -1,72 +1,94 @@
 <?php
 
 /**
- * @version $Id$
+ * @version 1.4
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @copyright Daniele Binaghi, 2018-2019
+ * @copyright Daniele Binaghi, 2018-2021
  * @package EmailNotification
  */
 
- 
-// Define Constants
-define('EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_SUBJECT', __('New Item added'));
-define('EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_MESSAGE', __('A new Item has been added to the Omeka repository.'));
-define('EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_SUBJECT', __('New Collection added'));
-define('EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_MESSAGE', __('A new Collection has been added to the Omeka repository.'));
-define('EMAIL_NOTIFICATION_NEW_EXHIBIT_EMAIL_SUBJECT', __('New Exhibit added'));
-define('EMAIL_NOTIFICATION_NEW_EXHIBIT_EMAIL_MESSAGE', __('A new Exhibit has been added to the Omeka repository.'));
-define('EMAIL_NOTIFICATION_MESSAGE_SENT', __('Admin/editors have been notified about the new addition.'));
-
 class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
 {
+	/**
+	 * @var array Hooks for the plugin.
+	 */
 	protected $_hooks = array(
 		'install',
 		'uninstall',
 		'initialize',
-		'config',
 		'config_form',
+		'config',
+		'before_save_item',
+		'before_save_collection',
+		'before_save_exhibit',
 		'after_save_item',
 		'after_save_collection',
 		'after_save_exhibit'
 	);
 
+	/**
+	 * Install the plugin.
+	 */
 	public function hookInstall()
 	{
-		set_option('email_notification_new_item', '0');	
-		set_option('email_notification_new_item_email_subject', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_SUBJECT);
-		set_option('email_notification_new_item_email_message', EMAIL_NOTIFICATION_NEW_ITEM_EMAIL_MESSAGE);
+		set_option('email_notification_new_item', '0');
+		set_option('email_notification_new_item_email_subject', __('New Item added'));
+		set_option('email_notification_new_item_email_message', __('A new Item has been added to the Omeka repository.'));
 		set_option('email_notification_new_collection', '0');
-		set_option('email_notification_new_collection_email_subject', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_SUBJECT);
-		set_option('email_notification_new_collection_email_message', EMAIL_NOTIFICATION_NEW_COLLECTION_EMAIL_MESSAGE);
+		set_option('email_notification_new_collection_email_subject', __('New Collection added'));
+		set_option('email_notification_new_collection_email_message', __('A new Collection has been added to the Omeka repository.'));
 		set_option('email_notification_new_exhibit', '0');
-		set_option('email_notification_new_exhibit_email_subject', EMAIL_NOTIFICATION_NEW_EXHIBIT_EMAIL_SUBJECT);
-		set_option('email_notification_new_exhibit_email_message', EMAIL_NOTIFICATION_NEW_EXHIBIT_EMAIL_MESSAGE);
+		set_option('email_notification_new_exhibit_email_subject', __('New Exhibit added'));
+		set_option('email_notification_new_exhibit_email_message', __('A new Exhibit has been added to the Omeka repository.'));
 		set_option('email_notification_recipient_address', get_option('administrator_email'));
 		set_option('email_notification_notify_editors', '0');
-		set_option('email_notification_message_sent', '0');
+		set_option('email_notification_notify_owner', '0');
+		set_option('email_notification_notification_alert', '0');
 	}
 
+	/**
+	 * Uninstall the plugin.
+	 */
 	public function hookUninstall()
 	{
 		delete_option('email_notification_new_item');
 		delete_option('email_notification_new_item_email_subject');
 		delete_option('email_notification_new_item_email_message');
-		delete_option('email_notification_new_collection');	
+		delete_option('email_notification_new_collection');
 		delete_option('email_notification_new_collection_email_subject');
 		delete_option('email_notification_new_collection_email_message');
-		delete_option('email_notification_new_exhibit');	
+		delete_option('email_notification_new_exhibit');
 		delete_option('email_notification_new_exhibit_email_subject');
 		delete_option('email_notification_new_exhibit_email_message');
 		delete_option('email_notification_recipient_address');
 		delete_option('email_notification_notify_editors');
-		delete_option('email_notification_message_sent');
+		delete_option('email_notification_notify_owner');
+		delete_option('email_notification_notification_alert');
 	 }
 
+	/**
+	 * Initializes the plugin.
+	 */
 	public function hookInitialize()
 	{
 		add_translation_source(dirname(__FILE__) . '/languages');
 	}
-	
+
+	/**
+	 * Shows plugin configuration page.
+	 *
+	 * @return void
+	 */
+	public function hookConfigForm()
+	{
+		include 'config_form.php';
+	}
+
+	/**
+	 * Processes the configuration form.
+	 *
+	 * @return void
+	 */
 	public function hookConfig($args)
 	{
 		$post = $args['post'];
@@ -81,45 +103,164 @@ class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
 		set_option('email_notification_new_exhibit_email_message', $post['email_notification_new_exhibit_email_message']);
 		set_option('email_notification_recipient_address', $post['email_notification_recipient_address']);
 		set_option('email_notification_notify_editors', $post['email_notification_notify_editors']);
-		set_option('email_notification_message_sent', $post['email_notification_message_sent']);
+		set_option('email_notification_notify_owner', $post['email_notification_notify_owner']);
+		set_option('email_notification_notification_alert', $post['email_notification_notification_alert']);
 	}
-	
-	public function hookConfigForm()
+
+	/**
+	 * Before save item hook.
+	 *
+	 * @param array $args
+	 */
+	public function hookBeforeSaveItem($args)
 	{
-		include 'config_form.php';
+		if (!$args['post'] || $args['insert'] == 1) {
+			// saving new item, so no action is required
+			return;
+		}
+
+		$record = $args['record'];
+		$item = get_record_by_id('Item', $record->id);
+
+		if ($item->public == 1) {
+			// item is already public, so no action is required
+			return;
+		}
+
+		if ($args['post']['public'] == 0) {
+			// item hasn't been made public, so no action is required
+			return;
+		}
+
+		if ($item->owner_id == current_user()->id) {
+			// item has been edited by creator, so no action is required
+			return;
+		}
+
+		// alert creator
+		$this->notifyCreator('item', $item);
 	}
-	
+
+	/**
+	 * Before save collection hook.
+	 *
+	 * @param array $args
+	 */
+	public function hookBeforeSaveCollection($args)
+	{
+		if (!$args['post'] || $args['insert'] == 1) {
+			// saving new collection, so no action is required
+			return;
+		}
+
+		$record = $args['record'];
+		$collection = get_record_by_id('Collection', $record->id);
+
+		if ($collection->public == 1) {
+			// collection is already public, so no action is required
+			return;
+		}
+
+		if ($args['post']['public'] == 0) {
+			// collection hasn't been made public, so no action is required
+			return;
+		}
+
+		if ($collection->owner_id == current_user()->id) {
+			// collection has been edited by creator, so no action is required
+			return;
+		}
+
+		// alert creator
+		$this->notifyCreator('collection', $collection);
+	}
+
+	/**
+	 * Before save exhibit hook.
+	 *
+	 * @param array $args
+	 */
+	public function hookBeforeSaveExhibit($args)
+	{
+		if (!$args['post'] || $args['insert'] == 1) {
+			// saving new exhibit, so no action is required
+			return;
+		}
+
+		$record = $args['record'];
+		$exhibit = get_record_by_id('Exhibit', $record->id);
+
+		if ($exhibit->public == 1) {
+			// exhibit is already public, so no action is required
+			return;
+		}
+
+		if ($args['post']['public'] == 0) {
+			// exhibit hasn't been made public, so no action is required
+			return;
+		}
+
+		if ($exhibit->owner_id == current_user()->id) {
+			// exhibit has been edited by creator, so no action is required
+			return;
+		}
+
+		// alert creator
+		$this->notifyCreator('exhibit', $exhibit);
+	}
+
+	/**
+	 * After save item hook.
+	 *
+	 * @param array $args
+	 */
 	public function hookAfterSaveItem($args)
 	{
 		if (!$args['post'] || $args['insert'] != 1) {
 			return;
 		}
-		$this->sendNotification('item', $args['record']);
+		$this->notifyUsers('item', $args['record']);
 	}
 
+	/**
+	 * After save collection hook.
+	 *
+	 * @param array $args
+	 */
 	public function hookAfterSaveCollection($args)
 	{
 		if (!$args['post'] || $args['insert'] != 1) {
 			return;
 		}
-		$this->sendNotification('collection', $args['record']);
+		$this->notifyUsers('collection', $args['record']);
 	}
 
+	/**
+	 * After save exhibit hook.
+	 *
+	 * @param array $args
+	 */
 	public function hookAfterSaveExhibit($args)
 	{
 		if (!$args['post'] || $args['insert'] != 1) {
 			return;
 		}
-		$this->sendNotification('exhibit', $args['record']);
+		$this->notifyUsers('exhibit', $args['record']);
 	}
 
-	public function sendNotification($type, $record) {
+	/**
+	 * Creates notification for users.
+	 *
+	 * @param string $type
+	 * @param array $record
+	 */
+	private function notifyUsers($type, $record) {
 		$recipientAddress = get_option('email_notification_recipient_address');
 		$notifyEditors = get_option('email_notification_notify_editors');
 		$bodyHtml = '';
 		$subject = '';
 		$bMessageSent = false;
-		
+
 		if ($recipientAddress != '' || $notifyEditors) {
 			// creates e-mail elements
 			$title = metadata($record, array('Dublin Core', 'Title'));
@@ -127,7 +268,7 @@ class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
 			$date = metadata($record, 'added');
 			$public = ($record->public == 1 ? __('public') : __('private'));
 			$featured = ($record->featured == 1 ? __('is featured') : __('is not featured'));
-			
+
 			if ($type == 'item' && get_option('email_notification_new_item')) {
 				$subject =  get_option('email_notification_new_item_email_subject');
 				$bodyHtml = get_option('email_notification_new_item_email_message');
@@ -136,14 +277,14 @@ class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
 				$collection_title = metadata($record, 'Collection Name');
 				if ($title == '') {
 					$title = __('not provided');
-					$message = __('No title has been provided for the new Item.');
+					$message = __('No title has been provided for the new Item');
 					$flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
 					$flash->addMessage($message, 'error');
 				}
 				$fields = array('{item_title}', '{item_creator}', '{item_creation_date}', '{item_collection_title}', '{item_public_status}', '{item_featured_status}', '{item_admin_url}', '{item_public_url}');
 				$values = array($title, $creator, $date, $collection_title, $public, $featured, $url_admin, $url_public);
 				$bodyHtml = str_replace($fields, $values, $bodyHtml);
-			} elseif ($type == 'collection' && get_option('email_notification_new_collection')) {	
+			} elseif ($type == 'collection' && get_option('email_notification_new_collection')) {
 				$subject =  get_option('email_notification_new_collection_email_subject');
 				$bodyHtml = get_option('email_notification_new_collection_email_message');
 				$url_admin = $_SERVER['HTTP_HOST'] . '/admin/collections/show/id/' . $record['id'];
@@ -172,45 +313,94 @@ class EmailNotificationPlugin extends Omeka_Plugin_AbstractPlugin
 				$values = array($title, $creator, $date, $public, $featured, $url_admin, $url_public);
 				$bodyHtml = str_replace($fields, $values, $bodyHtml);
 			}
-			
+
 			if ($bodyHtml != '' && $subject != '') {
 				$bodyHtml .= '<hr>' . __('This is an automatically generated message - please do not reply directly to this e-mail');
 				if ($recipientAddress != '') {
-					$recipientAddresses = explode(',', $recipientAddress);
 					// sends e-mail to recipient address(es)
-					$email = new Zend_Mail('utf-8');
-					$email->setBodyHtml($bodyHtml);
-					$email->setFrom(get_option('administrator_email'));
-					$email->addTo($recipientAddresses);
-					$email->setSubject($subject);
-					$email->send();
-					$bMessageSent = true;
+					$bMessageSent = $this->sendEmail(explode(',', $recipientAddress), $subject, $bodyHtml);
 				}
-				
+
 				if ($notifyEditors) {
 					// sends e-mail to all users with general 'edit' privilege
 					$acl = get_acl();
 					$users = get_db()->getTable('User')->findAll();
-					foreach ($users as $key => $user):
-						if ($acl->isAllowed($user, new Item, 'edit')):
-							$email = new Zend_Mail('utf-8');
-							$email->setBodyHtml($bodyHtml);
-							$email->setFrom(get_option('administrator_email'));
-							$email->addTo($user->email);
-							$email->setSubject($subject);
-							$email->send();
-							$bMessageSent = true;
-						endif;
-					endforeach;
+					foreach ($users as $key => $user) {
+						if ($acl->isAllowed($user, new Item, 'edit')) {
+							if ($this->sendEmail($user->email, $subject, $bodyHtml)) $bMessageSent = true;
+						}
+					}
 				}
-				
+
 				if (get_option('email_notification_message_sent') && $bMessageSent) {
 					// shows alert for new item/collection/exhibit creator
-					$message = __(EMAIL_NOTIFICATION_MESSAGE_SENT);
+					$message = __('Admin/editors have been notified about the new addition.');
 					$flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
 					$flash->addMessage($message, 'success');
 				}
 			}
 		}
+	}
+
+	/**
+	 * Creates notification for creator.
+	 *
+	 * @param string $type
+	 * @param array $record
+	 */
+	private function notifyCreator($type, $record) {
+		$bodyHtml = '';
+		$subject = '';
+		$recipientAddress = $record->getOwner()->email;
+
+		if ($recipientAddress != '') {
+			// creates e-mail elements
+			$title = metadata($record, array('Dublin Core', 'Title'));
+			$editor = current_user()->name;
+			$type_localized = __($type);
+			if (is_array($type_localized)) $type_localized = $type_localized[0];
+			$subject = __('%s made public', ucfirst($type_localized));
+			$url_public = $_SERVER['HTTP_HOST'] . '/' . $type . 's/show/id' . $record['id'];
+			$bodyHtml  = '<p>' . __('Hello!') . '</p>';
+			$bodyHtml .= '<p>' . __('Your %s "<b>%s</b>" has just been made public by <b>%s</b>.', $type_localized, $title, $editor) . '</p>';
+			$bodyHtml .= '<p>' . __('The %s is now available at the page %s.', $type_localized, $url_public) . '</p>';
+
+			if ($bodyHtml != '' && $subject != '') {
+				$bodyHtml .= '<hr>' . __('This is an automatically generated message - please do not reply directly to this e-mail');
+				if ($recipientAddress != '') {
+					// sends e-mail to recipient address
+					$bMessageSent = $this->sendEmail($recipientAddress, $subject, $bodyHtml);
+				}
+
+				if (get_option('email_notification_notification_alert') && $bMessageSent) {
+					// shows alert for publisher
+					$message = __('The owner of the %s has been notified of the publishing.', $type_localized);
+					$flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+					$flash->addMessage($message, 'success');
+				}
+			}
+		}
+	}
+
+	/**
+	 * Sends e-mail.
+	 *
+	 * @param string $recipient
+	 * @param string $subject
+	 * @param string $body
+	 *
+	 * returns boolean
+	 */
+	private function sendEmail($recipient, $subject, $body) {
+		if ($recipient == '' || $subject == '' || $body == '') return false;
+
+		$email = new Zend_Mail('utf-8');
+		$email->setFrom(get_option('administrator_email'))
+			->addTo($recipient)
+			->setSubject($subject)
+			->setBodyHtml($body)
+			->addHeader('X-Mailer', 'PHP/' . phpversion())
+			->send();
+		return true;
 	}
 }
